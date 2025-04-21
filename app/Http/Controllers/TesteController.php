50cf8b8561c4a64f8f01bee7721af956adf197bb
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-04-21 17:57:04 -0300

    Primeiro commit

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Ismaelw\LaraTeX\LaraTeX;
+
+class TesteController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        // return (new LaraTeX)->dryRun();
+        return (new LaraTeX('relatorios.tex'))
+                    ->with([
+                        'prefeitura' => 'PARNAIBA PIAUI'
+                    ])->inline('matricula.pdf');
+        // return (new LaraTeX('latex.tex'))->with([
+        //     'Name' => 'Luiz Lins',
+        //     'Dob' => '27/10/1985',
+        //     'SpecialCharacters' => '$ (a < b) $',
+        //     'languages' => [
+        //         'Português',
+        //         'Chinês',
+        //         'Espanhol'
+        //     ]
+        // ])->inline('test.pdf');
+        // ])->savePdf(storage_path('app/export/test.pdf'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TesteController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/latex', TesteController::class);
